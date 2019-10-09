3a565129c24383b924b8897fee8e13250344f02a
Shaqal7 <shaqal7@gmail.com>
2019-10-09 14:36:48 +0200

    Lookup Stock Details

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -11,12 +11,18 @@ class Stock < ApplicationRecord
 
   def self.new_from_lookup(ticker_symbol)
     begin
-      client = IEX::Api::Client.new(publishable_token: 'pk_421b4e44aa2343ba842d88e33280b5bb')
-      looked_up_stock = client.quote(ticker_symbol)
+      #client = IEX::Api::Client.new(publishable_token: 'pk_421b4e44aa2343ba842d88e33280b5bb')
+      client = StockQuote::Stock.new(api_key: 'pk_421b4e44aa2343ba842d88e33280b5bb')
+      looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+      #price = strip_commas(looked_up_stock.latest_price)
       new(name: looked_up_stock.company_name,
           ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
     rescue Exception => e
-      return nil
+      return e
     end
+  end
+
+  def self.strip_commas(number)
+    number.gsub(",","")
   end
 end
