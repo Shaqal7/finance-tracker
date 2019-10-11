5b01940527de7f6070ff5a220f351e4a9f4fc210
Shaqal7 <shaqal7@gmail.com>
2019-10-11 11:56:55 +0200

    Working errors and Refactoring for stock_controller

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,21 +1,11 @@
 class StocksController < ApplicationController
   def search
-    if params[:stock].present?
-      @stock = Stock.new_from_lookup(params[:stock])
-      if @stock
-        respond_to do |format|
-          format.js { render partial: 'users/result', stock: @stock }
-          #format.json { render :json => {:success => true, :html => (render_to_string 'users/result')} }
-        end
-        #render partial: 'users/result'
-        #render 'users/my_portfolio'
-      else
-        flash[:danger] = "You have entered an incorrect symbol"
-        redirect_to my_portfolio_path
-      end
+    if params[:stock].blank?
+      flash.now[:danger] = "You have entered an empty search string"
     else
-      flash[:danger] = "You have entered an empty search string"
-      redirect_to my_portfolio_path
+      @stock = Stock.new_from_lookup(params[:stock])
+      flash.now[:danger] = "You have entered an incorrect symbol" unless @stock
     end
+    render partial: 'users/result'
   end
 end
