823ef3cda66f7ef61a685ad372a6a5dceb8e7a89
Shaqal7 <shaqal7@gmail.com>
2019-10-11 14:52:06 +0200

    Add many to many associations - Users and Stocks, and save after search

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -11,18 +11,12 @@ class Stock < ApplicationRecord
 
   def self.new_from_lookup(ticker_symbol)
     begin
-      #client = IEX::Api::Client.new(publishable_token: 'pk_421b4e44aa2343ba842d88e33280b5bb')
       client = StockQuote::Stock.new(api_key: 'pk_421b4e44aa2343ba842d88e33280b5bb')
       looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
-      #price = strip_commas(looked_up_stock.latest_price)
       new(name: looked_up_stock.company_name,
           ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
     rescue Exception => e
       return nil
     end
-  end
-
-  def self.strip_commas(number)
-    number.gsub(",","")
   end
 end
