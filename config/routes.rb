92fb5f9f3366f5df681c3cd6fd558f3d82731819
Shaqal7 <shaqal7@gmail.com>
2019-10-14 14:57:58 +0200

    Add Friendship View

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'user_stocks/create'
-  devise_for :users, controllers: { registration: "user/registrations"}
+
+  devise_for :users, controllers: { registration: "user/registrations" }
+  resources :user_stocks, only: [:create, :destroy]
+  resources :users, only: [:show]
+  resources :friendships
+
   root 'welcome#index'
+  get 'user_stocks/create'
   get 'my_portfolio', to: "users#my_portfolio"
   get "search_stocks", to: "stocks#search"
-  resources :user_stocks, only: [:create, :destroy]
   get 'my_friends', to: "users#my_friends"
 end
